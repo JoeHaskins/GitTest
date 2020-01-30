d593855d508bef1385633ec7d46e01ab80c77caf
joehaskins <c18336951@mytudublin.ie>
2020-01-30 09:51:09 +0000

    Initial Version

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # GitTest
+
+1. list
+1. item
---
+++ src/ie/tudublin/Main.java
@@ -0,0 +1,12 @@
+package ie.tudublin;
+public class Main{
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+
+        for (String s:args) {
+            System.out.println(s);
+        }
+    }
+}
